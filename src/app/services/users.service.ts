3a3951183edf880ebb94e7bb42bdfaf0e42b75e3
subin <subinshrestha2@bajratechnologies.com>
2021-09-01 16:40:43 +0545

    Squashed commit of the following:
    commit 066962fe89e37985fe550074df0e9f955b995747
    Author: subin <subinshrestha2@bajratechnologies.com>
    Date:   Wed Sep 1 16:37:48 2021 +0545
    
        Squashed commit of the following:
    
        commit 77b99f06b62d1b04b0d135d08e1cffa299d49c21
        Author: subin <subinshrestha2@bajratechnologies.com>
        Date:   Wed Sep 1 16:32:41 2021 +0545
    
            FIX(Dashboard): Fixed services
    
            updated services to use local data insted of fetching it from http server
    
        commit 8ba268ec9af0cb8f05c4de4ae85b35d6ecb931b9
        Author: subin <subinshrestha2@bajratechnologies.com>
        Date:   Wed Sep 1 16:10:40 2021 +0545
    
            FIX(Dashboard): Overview Style fix
    
            fixed layout and styling of overview component in dashboard
    
        commit 7705994c3892a50277af4b5d1c219d316aa461c8
        Author: subin <subinshrestha2@bajratechnologies.com>
        Date:   Wed Sep 1 12:17:50 2021 +0545
    
            FEAT(Dashboard): Overview Secion Created
    
            Created and impemented overview section in dashboard
            only basic stying done
    
        commit 56a88878232cbfb457bc9e7432d5df3331178412
        Author: subin <subinshrestha2@bajratechnologies.com>
        Date:   Tue Aug 31 17:36:15 2021 +0545
    
            FEAT(Dashboard): Sub Header Created
    
            Sub header component generated and implemented
            add redirect link in header component
            added text color in global style
    
        commit 6409b585c724e47ca737e86a110952bdbd8dfc87
        Author: subin <subinshrestha2@bajratechnologies.com>
        Date:   Tue Aug 31 16:17:57 2021 +0545
    
            FEAT(Dashboard): Header componenet added
    
            generated and added header component in dashboard
    
    commit ad619fea006fd0469a4ab76d702111e98f4268ea
    Author: subin <subinshrestha2@bajratechnologies.com>
    Date:   Tue Aug 31 15:14:31 2021 +0545
    
        FEAT(ngZorro): Installed ng Zorro
    
        installed and simpe implementation of ng zorro
    
    commit 1c9a6755c6ce208da05bbb371f68f85ef985fd71
    Author: subin <subinshrestha2@bajratechnologies.com>
    Date:   Tue Aug 31 14:59:51 2021 +0545
    
        FEAT(Http): Tasks added in dashboard
    
        simple tasks layout added in dashboard
    
    commit af4a68beaea8f5e779439814cd10322e9b43538d
    Author: subin <subinshrestha2@bajratechnologies.com>
    Date:   Mon Aug 30 18:10:25 2021 +0545
    
        FEAT(Http): Get request added
    
        get request for tasks added
        displayed tasks in dashboard
    
    commit d1b2771cfb14b0574f59467d398636ff8d9291e5
    Author: subin <subinshrestha2@bajratechnologies.com>
    Date:   Mon Aug 30 16:34:25 2021 +0545
    
        FEAT(Http): Create Task Added
    
        Create task service created
        New create task adds entry in the json-server
    
    commit 27b6fe304e0fcd73533bbb777fa18ffb1927c7ce
    Author: subin <subinshrestha2@bajratechnologies.com>
    Date:   Mon Aug 30 14:38:52 2021 +0545
    
        FEAT(Http): JSON server created
    
        Created a mock json server using json-server
        used json server to host userlist
        updated services subscribe to http client

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@angular/core';
-import { User } from '../user';
+import { User, UserList } from '../user';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UsersService {
-  private apiUrl = 'http://localhost:5000/users';
-  constructor(private http: HttpClient) {}
-
+  //with local json server
+  //   private apiUrl = 'http://localhost:5000/users';
+  //   constructor(private http: HttpClient) {}
+  //   getUser(): Observable<User[]> {
+  //     return this.http.get<User[]>(this.apiUrl);
+  //   }
   getUser(): Observable<User[]> {
-    return this.http.get<User[]>(this.apiUrl);
+    const userList = of(UserList);
+    return userList;
   }
 }
--- src/app/tasks.ts
+++ src/app/tasks.ts
@@ -4,3 +4,18 @@ export interface Task {
   priority: string;
   dateTime: string;
 }
+
+export const TaskList: Task[] = [
+  {
+    name: 'Assignment',
+    list: 'College',
+    priority: 'Medium',
+    dateTime: '2021-08-12T16:04',
+  },
+  {
+    name: 'Reseach',
+    list: 'Personal',
+    priority: 'Medium',
+    dateTime: '2021-08-31T16:49',
+  },
+];
