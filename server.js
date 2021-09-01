f165aa89589f76e63feb905f6b1fc3f444a8a95e
subin <subinshrestha2@bajratechnologies.com>
2021-09-01 12:50:03 +0545

    FEAT(Heroku): redirect file added

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
-//Install express server
-const express = require("express");
 const path = require("path");
-
+const express = require("express");
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + "./dist/todoapp/"));
-
+app.use(express.static("dist" + "/todoapp"));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/todoapp/index.html"));
+  res.sendFile(path.join("dist/", "todoapp", "index.html"));
 });
-
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
