77b99f06b62d1b04b0d135d08e1cffa299d49c21
subin <subinshrestha2@bajratechnologies.com>
2021-09-01 16:32:41 +0545

    FIX(Dashboard): Fixed services
    updated services to use local data insted of fetching it from http server

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -1,27 +1,34 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Task } from '../tasks';
+import { Observable, of } from 'rxjs';
+import { Task, TaskList } from '../tasks';
 
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type': 'application/json',
-  }),
-};
+//for local json server
+// const httpOptions = {
+//   headers: new HttpHeaders({
+//     'Content-Type': 'application/json',
+//   }),
+// };
 
 @Injectable({
   providedIn: 'root',
 })
 export class TasksService {
-  private apiUrl = 'http://localhost:5000/tasks';
-
-  constructor(private http: HttpClient) {}
-
+  //For local json server
+  //   private apiUrl = 'http://localhost:5000/tasks';
+  //   constructor(private http: HttpClient) {}
+  //   getTasks(): Observable<Task[]> {
+  //     return this.http.get<Task[]>(this.apiUrl);
+  //   }
+  //   addTask(task: Task): Observable<Task> {
+  //     return this.http.post<Task>(this.apiUrl, task, httpOptions);
+  //   }
   getTasks(): Observable<Task[]> {
-    return this.http.get<Task[]>(this.apiUrl);
+    let taskList = of(TaskList);
+    return taskList;
   }
-
   addTask(task: Task): Observable<Task> {
-    return this.http.post<Task>(this.apiUrl, task, httpOptions);
+    TaskList.push(task);
+    return of(task);
   }
 }
