33953bed29dac7b127d9999e144cbb395b2f7b6c
subin <subinshrestha2@bajratechnologies.com>
2021-09-20 14:16:05 +0545

    FEAT(login): login test added
    Added unit test for login component and users service component
    used karma and jasmine

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,6 +1,13 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, tick } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { UsersService } from 'src/app/services/users.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ThemePalette } from '@angular/material/core';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,9 +15,15 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
+      imports: [
+        ReactiveFormsModule,
+        FormsModule,
+        HttpClientTestingModule,
+        RouterTestingModule,
+      ],
+      declarations: [LoginComponent],
+      providers: [{ provide: UsersService }],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +34,28 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have color theme to be `accent`', () => {
+    let temp: ThemePalette = 'accent';
+    expect(component.color).toBe(temp);
+  });
+
+  it('should have submit to be false', () => {
+    expect(component.submit).toEqual(false);
+  });
+
+  it('should have showPassword to be false', () => {
+    expect(component.showPassword).toEqual(false);
+  });
+
+  it('should toggle showPassword', () => {
+    expect(component.showPassword).toEqual(false);
+    component.showHidePassword();
+    expect(component.showPassword).toEqual(true);
+  });
+
+  it('should define users', () => {
+    expect(component.users).toBeDefined();
   });
 });
--- src/app/services/users.service.spec.ts
+++ src/app/services/users.service.spec.ts
@@ -1,16 +1,29 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UsersService } from './users.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(UsersService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should be defined', () => {
+    service.getUser().subscribe((res) => {
+      expect(res).toBeDefined();
+      expect(res[0].email).toEqual('Joe_Abernathy@hotmail.com');
+      expect(res[0].name).toEqual('name 1');
+      expect(res[0].gender).toEqual('Female');
+      expect(res[0].dob).toEqual(new Date('2055-06-22T23:11:39.441Z'));
+    });
   });
 });
