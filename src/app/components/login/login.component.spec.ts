cfd505e4d3b7324132f05d5e86bc5999d3cb47ed
subin <subinshrestha2@bajratechnologies.com>
2021-09-21 18:01:36 +0545

    feat(login): login form submit test added added unit test for error messages after form test added validation test after form submit "

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,4 +1,10 @@
-import { ComponentFixture, TestBed, tick } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  fakeAsync,
+  flush,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
 
@@ -8,10 +14,13 @@ import { UsersService } from 'src/app/services/users.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ThemePalette } from '@angular/material/core';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let de: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -29,6 +38,8 @@ describe('LoginComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
+
     fixture.detectChanges();
   });
 
@@ -58,4 +69,60 @@ describe('LoginComponent', () => {
   it('should define users', () => {
     expect(component.users).toBeDefined();
   });
+
+  it('should display error message', () => {
+    component.formSubmit();
+    let test = de.queryAll(By.css('.invalid-text'));
+    test.forEach((t) =>
+      expect(t.nativeElement.innerText).toEqual(' Invalid Input ')
+    );
+  });
+
+  it('should display error message on form submit', fakeAsync(() => {
+    expect(component.submit).toEqual(false);
+    expect(component.formValid).toEqual(false);
+
+    component.submit = true;
+    component.formValid = true;
+
+    fixture.detectChanges();
+
+    expect(component.submit).toEqual(true);
+    expect(component.formValid).toEqual(true);
+    //  expect(component.myForm.valid).toBeFalsy();
+
+    let test = de.queryAll(By.css('.invalid-text'));
+    let errors = [
+      'Incorrect Password !',
+      ' Invalid Input ',
+      'Incorrect Email Address !',
+      ' Invalid Input ',
+    ];
+
+    errors.forEach((error) => {
+      expect(test.pop().nativeElement.innerText).toEqual(error);
+    });
+  }));
+
+  it('shoud check when form is valid', fakeAsync(() => {
+    expect(component.myForm.valid).toBeFalse();
+
+    component.myForm.controls['email'].setValue('subin@hotmail.com');
+    component.myForm.controls['password'].setValue('Password1');
+    flush();
+    fixture.detectChanges();
+    expect(component.myForm.valid).toBeTrue();
+    expect(component.myForm.invalid).toBeFalse();
+  }));
+
+  it('shoud check when form is invalid', fakeAsync(() => {
+    expect(component.myForm.valid).toBeFalse();
+
+    component.myForm.controls['email'].setValue('subin@hotmail');
+    component.myForm.controls['password'].setValue('Password123');
+    flush();
+    fixture.detectChanges();
+    expect(component.myForm.valid).toBeFalse();
+    expect(component.myForm.invalid).toBeTrue();
+  }));
 });
